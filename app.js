78ffaacfacb27d00e1337fdafb93d9d47695260a
Caroline Leopold <caroline.leopold@gmail.com>
2021-03-23 11:25:12 -0400

    hbs install and template editing

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const exphbs = require('express-handlebars');
+
+const port = 5000
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -10,8 +13,21 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 // view engine setup
+
+app.set('view engine', 'hbs');
+
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: __dirname + '/views/layouts',
+  extname: '.hbs'
+}));
+
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,6 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use("/public", express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -37,5 +54,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//Makes the app listen to port 3000
+app.listen(port, () => console.log(`App listening to port ${port}`));
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,22 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+}); 
+
+/*
+
+router.get('/', (req, res) => {
+  res.render('home');
 });
+
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+router.get('/', (req, res) => {
+  res.render('contact');
+});
+
+*/ 
 
 module.exports = router;
